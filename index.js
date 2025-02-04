3302c2b402768254ca3c13cf2416d8abd3a2d074
ohbeomho <ohbeomho0322@proton.me>
2025-02-04 20:13:15 +0900

    Reddit json api

---
+++ index.js
@@ -0,0 +1,4 @@
+// Get subreddit info from https://www.reddit.com/r/[subreddit]/about.json
+
+// Get subreddit posts from https://www.reddit.com/r/[subreddit].json
+// With different sort option: .../[subreddit]/[sort].json
